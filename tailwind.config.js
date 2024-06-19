41bc39d9217688820097e228d128dc1c0f437340
Svaym <162036192+Svaym@users.noreply.github.com>
2024-06-19 16:39:37 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens : {
+      "5xl" : { max: "1920px" },
+      "4xl" : { max: "1870px" },
+      "3xl" : { max: "1680px" },
+      "2xl" : { max: "1400px" },
+      "xl" : { max: "1200px" },
+      "lg" : { max: "992px" },
+      "md" : { max: "768px" },
+      "sm" : { max: "576px" },
+      "xs" : { max: "450px" },
+      "xxs" : { max: "375px" },
+    },
     extend: {},
   },
   plugins: [],
