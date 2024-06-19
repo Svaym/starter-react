f3dba8fdc05681673fa69bc815beef9415fbbebf
Svaym <Vaymeraa22@mail.ru>
2024-06-19 13:55:23 +0500

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-yellow-300 font-bold underline">
+    <h1 className="text-3xl text-orange-300 font-bold underline">
       Hello world!
     </h1>
   )
